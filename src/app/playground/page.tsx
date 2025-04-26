8dee4327d11b1d66e645c1858155a4031d7a997d
Seyit Yahya KURU <seyityahya05@gmail.com>
2025-04-26 05:48:35 -0400

    package.json bugfix

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,13 +1,46 @@
 "use client";
 import CheckBox from "@/components/core/checkbox";
 import InputBox from "@/components/core/input";
+import { DialogBase } from "@/components/core/dialog";
+import { Button } from "@/components/ui/button";
+import { CustomSelect, GroupedSelect } from "@/components/core/select";
 import { Eye, EyeOff, Lock, Mail } from "lucide-react";
 import { useState } from "react";
+
+const fruitOptions = [
+  { label: "Apple", value: "apple" },
+  { label: "Banana", value: "banana" },
+  { label: "Blueberry", value: "blueberry" },
+  { label: "Grapes", value: "grapes" },
+  { label: "Pineapple", value: "pineapple" },
+];
+
+const groupedOptions = [
+  {
+    label: "Fruits",
+    options: [
+      { label: "Apple", value: "apple" },
+      { label: "Banana", value: "banana" },
+      { label: "Grapes", value: "grapes" },
+    ],
+  },
+  {
+    label: "Vegetables",
+    options: [
+      { label: "Carrot", value: "carrot" },
+      { label: "Broccoli", value: "broccoli" },
+      { label: "Spinach", value: "spinach" },
+    ],
+  },
+];
 
 export default function Playground() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [selectedFruit, setSelectedFruit] = useState("");
+  const [selectedValue, setSelectedValue] = useState("");
+  const [openDialog, setOpenDialog] = useState(false);
 
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -45,24 +78,27 @@ export default function Playground() {
           Input Component
         </h2>
         <div className="flex flex-col gap-4">
-          <InputBox placeholder="Adınızı girin" />
+          <InputBox placeholder="Adınızı girin" className="max-w-sm" />
           <InputBox
             id="name"
             label="Ad"
             placeholder="Adınızı girin"
             error="Bu alan zorunludur"
+            className="max-w-sm"
           />
           <InputBox
             id="email"
             label="E-posta"
             placeholder="E-posta adresinizi girin"
             error="Geçerli bir e-posta girin"
+            className="max-w-sm"
           />
           <InputBox
             id="password"
             label="Şifre"
             placeholder="Şifrenizi girin"
             type="password"
+            className="max-w-sm"
           />
           <InputBox
             id="email"
@@ -70,6 +106,7 @@ export default function Playground() {
             placeholder="E-posta adresinizi girin"
             type="email"
             icon={<Mail size={18} />}
+            className="max-w-sm"
           />
           <InputBox
             id="password"
@@ -80,6 +117,7 @@ export default function Playground() {
             value={password}
             onChange={handlePasswordChange}
             error={error}
+            className="max-w-sm"
             rightIcon={
               <button
                 type="button"
@@ -90,6 +128,62 @@ export default function Playground() {
               </button>
             }
           />
+        </div>
+      </div>
+      {/* select */}
+      <div className="bg-card shadow-lg rounded-lg p-6 w-full max-w-3xl gap-8">
+        <h2 className="text-xl font-bold mb-4 text-purple-600 dark:text-purple-400">
+          Input Component
+        </h2>
+        <div className="flex flex-col gap-4">
+          <CustomSelect
+            dataSource={fruitOptions}
+            value={selectedFruit}
+            onChange={setSelectedFruit}
+            placeholder="Select a fruit"
+            label="Fruits"
+          />
+          <GroupedSelect
+            data={groupedOptions}
+            value={selectedValue}
+            onChange={setSelectedValue}
+          />
+        </div>
+      </div>
+
+      {/* Dialog */}
+      <div className="bg-card shadow-lg rounded-lg p-6 w-full max-w-3xl gap-8">
+        <h2 className="text-xl font-bold mb-4 text-purple-600 dark:text-purple-400">
+          Dialog Component
+        </h2>
+        <div className="flex flex-col gap-4">
+          <Button onClick={() => setOpenDialog(true)}>Open Dialog</Button>
+          <DialogBase
+            open={openDialog}
+            onOpenChange={setOpenDialog}
+            title="Edit profile"
+            description="Make changes to your profile here. Click save when you're done."
+            footer={<Button type="submit">Save changes</Button>}
+          >
+            <div className="grid gap-4">
+              <div className="grid items-center gap-4 width-full">
+                <InputBox
+                  id="name"
+                  label="name"
+                  placeholder="name"
+                  className="max-w-sm"
+                />
+              </div>
+              <div className="grid items-center gap-4 width-full">
+                <InputBox
+                  id="username"
+                  label="username"
+                  placeholder="username"
+                  className="max-w-md"
+                />
+              </div>
+            </div>
+          </DialogBase>
         </div>
       </div>
     </div>
